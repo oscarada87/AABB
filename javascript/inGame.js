fec2d31023946b929463b37c1bcf10fae2fff178
Oscar <oscarada87@gmail.com>
2018-02-02 17:20:13 +0800

    finish input output and display

--- javascript/inGame.js
+++ javascript/inGame.js
@@ -1,3 +1,6 @@
+let inputNumberArray = [];
+let globalInputCounter = 0;
+
 
 const getRandomNumber = () =>{
   let maxNum = 9;
@@ -31,4 +34,63 @@ const random4Numbers = () =>{
   return arr;
 }
 
-console.log(random4Numbers());
+//console.log(random4Numbers());  //use for debug
+
+/*let tempInput = document.getElementsByClassName('inputNumberBtn');
+let inputNumberButton = [tempInput[9]];
+for(let i = 0; i <= 8; i++){
+  inputNumberButton.push(tempInput[i]);
+}*/
+
+//Input Output
+const storeInput = (number) =>{
+  if(globalInputCounter >= 4){
+    return false;
+  }
+  inputNumberArray.push(number);
+  display();
+  globalInputCounter++;
+}
+
+const deleteInput = () =>{
+  if(globalInputCounter >= 1){
+    inputNumberArray.pop();
+    globalInputCounter--;
+    display();
+  }
+}
+
+const display = () =>{
+  let displayName = ('display'+ globalInputCounter);
+  let displayTarget = document.getElementById(displayName);
+  if(inputNumberArray[globalInputCounter] != null){
+    displayTarget.innerHTML = inputNumberArray[globalInputCounter];
+  }
+  else{
+    displayTarget.innerHTML = "-";
+  }
+}
+
+let inputBox = document.querySelector('.numberInputColumn');
+inputBox.addEventListener('click', (e) => {
+  let target = e.target;
+  if (target.type === 'button') {
+    storeInput(target.innerHTML);
+    console.log(inputNumberArray);
+  }
+});
+
+let submitBox = document.querySelector('.submit');
+submitBox.addEventListener('click', (e) =>{
+  let target = e.target;
+  if (target.type === 'button'){
+    if (target.innerHTML === '清除'){
+      deleteInput();
+      console.log(inputNumberArray);
+    }
+    else if(target.innerHTML === '送出'){
+      console.log("送出");
+      console.log(inputNumberArray);
+    }
+  }
+})
